31d28cd823018ef57b2a1f9a9cb85f78c0ae5efa
MUJAHIDmd <muzammilk8035@gmail.com>
2025-10-06 02:31:05 +0500

    done

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-    title: "GoCart. - Shop smarter",
-    description: "GoCart. - Shop smarter",
+    title: "VoltStore. - Shop smarter",
+    description: "VoltStore. - Shop smarter",
 };
 
 export default function RootLayout({ children }) {
--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,8 +1,8 @@
 import StoreLayout from "@/components/store/StoreLayout";
 
 export const metadata = {
-    title: "GoCart. - Store Dashboard",
-    description: "GoCart. - Store Dashboard",
+    title: "VoltStore. - Store Dashboard",
+    description: "VoltStore. - Store Dashboard",
 };
 
 export default function RootAdminLayout({ children }) {
